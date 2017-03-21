ad5a835e9b5cd320d2f8d93bfa9040911c8464c6
guxiangfly <964999133@qq.com>
2017-03-21 11:01:17 +0800

    添加ssh配置文件

---
+++ demo1/src/com/guxiang/web/action/LoginAction.java
@@ -0,0 +1,10 @@
+package com.guxiang.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by guxiang on 2017/3/21.
+ */
+public class LoginAction extends ActionSupport{
+
+}
